46dc9965917a973451f6c8aacb2166f5d742d521
yaoc1996 <yaoc1996@gmail.com>
2018-03-13 18:09:05 -0400

    fixed failed check for /

--- frontend/geddit/src/lib/verifyQueryString.js
+++ frontend/geddit/src/lib/verifyQueryString.js
@@ -3,7 +3,7 @@ import _ from 'lodash/fp';
 const verifyQueryString = _.flow(
   _.replace('?', ''),
   _.split('&'),
-  _.map(x => _.split('=')(x).length === 2),
+  _.map(x => _.split('=')(x).length === 2 || x === ''),
   _.reduce((x, y) => x && y)(true),
 )
 
