3b923eef6b70faa5ba2b97ac7a05f48137eb7fc9
Yong-L <dkrcjs7521@gmail.com>
2018-03-13 18:47:13 -0400

    Changed formatting

--- backend/users/user.js
+++ backend/users/user.js
@@ -4,46 +4,48 @@ const db = require('../config/db');
 
 db.connect();
 
-exports.createUser = function(user, done) {
-  db.get().query('INSERT INTO users SET ?', [user], (err, result) => {
-    if (err) throw err;
-    const newUser = {
-      userID: result.userID,
-      username: result.username,
-      password: result.password,
-      email: result.email
-    };
-    done(newUser);
-  })
-}
 
-exports.getUser = function(username, done) {
-  db.get().query('SELECT * FROM users WHERE username = ? LIMIT 1', [username], (err, rows, fields) => {
-    if (err) throw err;
-    done(rows[0]);
-  });
-}
+module.exports = {
+  createUser : (user, done) => {
+    db.get().query('INSERT INTO users SET ?', [user], (err, result) => {
+      if (err) throw err;
+      const newUser = {
+        userID: result.userID,
+        username: result.username,
+        password: result.password,
+        email: result.email
+      };
+      done(newUser);
+    })
+  },
 
-exports.getUserByID = function(userID, done) {
-  //console.log(userID);
-  db.get().query('SELECT * FROM users WHERE userID = ? LIMIT 1', [userID], (err, rows, fields) => {
-    if (err) throw err;
-    //console.log(rows);
-    done(rows[0]);
-  });
-}
+  getUser : (username, done) => {
+    db.get().query('SELECT * FROM users WHERE username = ? LIMIT 1', [username], (err, rows, fields) => {
+      if (err) throw err;
+      done(rows[0]);
+    });
+  },
 
-exports.emailExists = function(email, done) {
-  db.get().query('SELECT * FROM users WHERE email = ? LIMIT 1', [email], (err, rows, fields) => {
-    if (err) throw err;
-    done(rows.length > 0);
-  })
-}
+  getUserByID : (userID, done) => {
+    //console.log(userID);
+    db.get().query('SELECT * FROM users WHERE userID = ? LIMIT 1', [userID], (err, rows, fields) => {
+      if (err) throw err;
+      //console.log(rows);
+      done(rows[0]);
+    });
+  },
 
-exports.usernameExists = function(username, done) {
-  db.get().query('SELECT * FROM users WHERE username = ? LIMIT 1', [username], (err, rows, fields) => {
-    if (err) throw err;
-    done(rows.length > 0);
-  })
-}
+  emailExists : (email, done) => {
+    db.get().query('SELECT * FROM users WHERE email = ? LIMIT 1', [email], (err, rows, fields) => {
+      if (err) throw err;
+      done(rows.length > 0);
+    })
+  },
 
+  usernameExists : (username, done) => {
+    db.get().query('SELECT * FROM users WHERE username = ? LIMIT 1', [username], (err, rows, fields) => {
+      if (err) throw err;
+      done(rows.length > 0);
+    })
+  }
+};
