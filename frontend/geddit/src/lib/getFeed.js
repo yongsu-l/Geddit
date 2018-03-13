77b8f75b51b71f0e3d82d0d27c09ac50b491a58b
yaoc1996 <yaoc1996@gmail.com>
2018-03-13 11:39:33 -0400

    renamed

---
+++ frontend/geddit/src/lib/getFeed.js
@@ -0,0 +1,11 @@
+import constructQueryString from './constructQueryString';
+
+function getFeed(params) {
+  return (
+    fetch(`/feed?${constructQueryString(params)}`)
+      .then(res => res.json())
+      .catch(e => console.log(e))
+  )
+}
+
+export default getFeed;
