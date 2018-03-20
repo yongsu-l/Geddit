df59f14689384ffdcfc2de5cc11b64c3e137a67b
yaoc1996 <yaoc1996@gmail.com>
2018-03-20 03:43:49 -0400

    post request to create a new comment

---
+++ frontend/geddit/src/lib/postComment.js
@@ -0,0 +1,20 @@
+function postComment({ token, postID, parentID=null, content }) {
+  return (
+    fetch('/post/comment', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': token,
+      },
+      body: JSON.stringify({
+        postID,
+        parentID,
+        content,
+      })
+    })
+      .then(res => res.json())
+      .catch(e => console.log(e))
+  )
+}
+
+export default postComment;
