7ab5b98c064326df5730a464728c1306c514497b
Yong-L <dkrcjs7521@gmail.com>
2018-03-21 15:59:45 -0400

    Make comments now returns the new comment with username

--- backend/posts/comments/comment.js
+++ backend/posts/comments/comment.js
@@ -8,7 +8,11 @@ module.exports = {
   makeComment : (comment, done) => {
     db.get().query('INSERT INTO comments SET ?', [comment], (err, result) => {
       if (err) throw err;
-      done(result);
+      db.get().query(`SELECT commentID, username, content, dateCreated 
+                      FROM comments NATURAL JOIN users WHERE commentID = ? LIMIT 1`, [result.insertId], (err, row, result) => {
+        if (err) throw err;
+        done(row[0]);
+      });
     });
   }
 }
