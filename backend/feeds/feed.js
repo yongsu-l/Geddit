4af8c511eff7229ae49a91db328045cb30565768
Yong-L <dkrcjs7521@gmail.com>
2018-03-22 11:33:54 -0400

    Feed returns postCount for the length of posts for the number of posts for current page

--- backend/feeds/feed.js
+++ backend/feeds/feed.js
@@ -6,13 +6,15 @@ db.connect();
 
 module.exports = {
   getNewFeed(page, done) {
-    db.get().query('SELECT * FROM posts ORDER BY dateCreated DESC LIMIT 20 OFFSET ?', [(page - 1) * 20],(err, rows, fields) => {
+    db.get().query(`SELECT postID, title, username, content, dateCreated, votes 
+    from posts NATURAL JOIN users ORDER BY dateCreated DESC LIMIT 20 OFFSET ?`, [(page - 1) * 20], (err, rows, fields) => {
       if (err) throw err;
       done(rows);
     });
   },
   getTopFeed(page, done) {
-    db.get().query('SELECT * FROM posts ORDER BY votes DESC LIMIT 20 OFFSET ?', [(page - 1) * 20], (err, rows, fields) => {
+    db.get().query(`SELECT postID, title, username, content, dateCreated, votes 
+    FROM posts NATURAL JOIN users ORDER BY votes DESC LIMIT 20 OFFSET ?`, [(page - 1) * 20], (err, rows, fields) => {
       if (err) throw err;
       done(rows);
     });
--- backend/feeds/feedController.js
+++ backend/feeds/feedController.js
@@ -18,12 +18,12 @@ router.get('/', (req, res) => {
   else if (req.query.type === 'new') {
     query.getNewFeed(req.query.page, (posts) => {
       if (posts.length === 0) res.status(400).json({success: false, msg: 'No posts returned'});
-      else res.status(200).json({success: true, posts});
+      else res.status(200).json({success: true, posts, postCount: posts.length});
     })
   } else if (req.query.type === 'top') {
     query.getTopFeed(req.query.page, (posts) => {
       if (posts.length === 0) res.status(400).json({success: false, msg: 'No posts returned'});
-      else res.status(200).json({success: true, posts});
+      else res.status(200).json({success: true, posts, postCount: post.length});
     })
   } else {
     res.status(404).json({success: false, msg: 'Failed'});
