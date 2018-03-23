2d71aa18baafad41a1c5bb4e2f4a013a9c976c11
Yong-L <dkrcjs7521@gmail.com>
2018-03-23 19:22:07 -0400

    Cors

--- backend/config/server.js
+++ backend/config/server.js
@@ -6,11 +6,34 @@ const app         = require('express')();
 const bodyParser  = require('body-parser');
 const colors      = require('colors');
 const db          = require('./db');
+const cors        = require('cors');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Welcome to Geddit REST API'));
+
+app.use(cors());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'localhost:80');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 //API Routes 
 const user = require('../users');
